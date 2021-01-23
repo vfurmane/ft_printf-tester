324abec5579918a48243224095690bf56f59c1d1
vfurmane <vfurmane@student.42.fr>
2021-01-23 12:42:52 +0100

    Add negative precision and 0 flag test on negative number

--- test/srcs/expected/int_test.c
+++ test/srcs/expected/int_test.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 13:06:59 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/01/22 14:40:59 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/01/23 12:08:25 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ int	main(void)
 	printf("26. Returned value	: %d\n", printf("%07.4d\n", nbr));
 	printf("27. Returned value	: %d\n", printf("%-.7d\n", nbr));
 	printf("28. Returned value	: %d\n", printf("%.*d\n", 7, nbr));
-	printf("29. Returned value	: %d\n", printf("%*.*d\n", -3, -3, nbr));
+	printf("29. Returned value	: %d\n", printf("%*.*d %0*.*d\n", -3, -3, nbr, 4, -2, -nbr));
 	printf("30. Returned value	: %d\n", printf("%.*d %.*d %.*d\n", 7, -nbr, 3, -nbr, -4, nbr));
 	printf("31. Returned value	: %d\n", printf("%01.0d, %03.0d, %05.0d\n", 0, 0, nbr));
 	printf("32. WPL - Returned value	: %d\n", printf("WPL - %Hello Wo%rld\n"));
--- test/srcs/user/int_test.c
+++ test/srcs/user/int_test.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 13:06:59 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/01/22 14:42:32 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/01/23 12:08:15 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int	main(void)
 	ft_printf("26. Returned value	: %d\n", ft_printf("%07.4d\n", nbr));
 	ft_printf("27. Returned value	: %d\n", ft_printf("%-.7d\n", nbr));
 	ft_printf("28. Returned value	: %d\n", ft_printf("%.*d\n", 7, nbr));
-	ft_printf("29. Returned value	: %d\n", ft_printf("%*.*d\n", -3, -3, nbr));
+	ft_printf("29. Returned value	: %d\n", ft_printf("%*.*d %0*.*d\n", -3, -3, nbr, 4, -2, -nbr));
 	ft_printf("30. Returned value	: %d\n", ft_printf("%.*d %.*d %.*d\n", 7, -nbr, 3, -nbr, -4, nbr));
 	ft_printf("31. Returned value	: %d\n", ft_printf("%01.0d, %03.0d, %05.0d\n", 0, 0, nbr));
 	ft_printf("32. WPL - Returned value	: %d\n", ft_printf("WPL - %Hello Wo%rld\n"));
